4285af6c7e0c8b3ca735302b7ca9248876e9ba4f
eghammond <143139872+eghammond@users.noreply.github.com>
2026-06-06 00:06:18 -0700

    made database url an environment variable

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://eghammond@localhost/typingstats"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
